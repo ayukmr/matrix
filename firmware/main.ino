33c1825947e012f38cfe47fd0fa0a21197d78e9c
Ayush Kumar <github@ayukmr.com>
2026-01-02 13:25:07 -0800

    Replace components

--- firmware/main.ino
+++ firmware/main.ino
@@ -2,17 +2,17 @@
 #include <utility>
 #include <algorithm>
 
-std::array<int, 6> pins = {4, 5, 6, 7, 8, 9};
+std::array<int, 6> pins = {1, 3, 4, 5, 6, 7};
 
-std::array<std::pair<int, int>, 15> pairs = {
-    {2, 0}, {4, 2}, {5, 4},
-    {2, 1}, {4, 0}, {5, 2},
-    {4, 1}, {5, 0}, {3, 2},
-    {4, 3}, {5, 1}, {3, 0},
-    {5, 3}, {3, 1}, {1, 0}
-};
+std::array<std::array<std::pair<int, int>, 3>, 5> pairs = {{
+    {{{2, 0}, {4, 2}, {5, 4}}},
+    {{{2, 1}, {4, 0}, {5, 2}}},
+    {{{4, 1}, {5, 0}, {3, 2}}},
+    {{{4, 3}, {5, 1}, {3, 0}}},
+    {{{5, 3}, {3, 1}, {1, 0}}}
+}};
 
-std::array<bool, 30> leds{};
+std::array<std::array<bool, 6>, 5> leds{};
 
 void setup() {
     for (int pin : pins) {
@@ -21,22 +21,20 @@ void setup() {
 }
 
 void loop() {
-    for (int i = 0; i < 15; i++) {
-        int hi, lo;
-        std::tie(hi, lo) = pairs[i];
+    for (int y = 0; y < 5; y++) {
+        for (int x = 0; x < 3; x++) {
+            int hi, lo;
+            std::tie(hi, lo) = pairs[y][x];
 
-        for (int j = 0; j < 2; j++) {
-            bool on = leds[i * 2 + j];
+            for (int i = 0; i < 2; i++) {
+                bool on = leds[y][5 - (x * 2 + i)];
 
-            digitalWrite(pins[hi], on ? HIGH : LOW);
-            digitalWrite(pins[lo], on ? LOW : HIGH);
+                digitalWrite(pins[hi], on ? HIGH : LOW);
+                digitalWrite(pins[lo], on ? LOW : HIGH);
 
-            delay(10);
-            std::swap(hi, lo);
+                delay(10);
+                std::swap(hi, lo);
+            }
         }
     }
-}
-
-void led(int x, int y, bool v) {
-    leds[x + y * 6] = v;
 }
--- firmware/test.ino
+++ firmware/test.ino
@@ -1,7 +1,7 @@
 void setup() {
   pinMode(4, OUTPUT);
-  pinMode(6, OUTPUT);
+  pinMode(1, OUTPUT);
 
   digitalWrite(4, LOW);
-  digitalWrite(6, HIGH);
+  digitalWrite(1, HIGH);
 }
